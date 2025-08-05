93b28826df05bb3bf240eb482645df71f172acab
Petr Bulánek <bulanek.petr@gmail.com>
2025-08-05 10:20:31 +0200

    fix(ui): useSearchParams needs to be wrapped into Suspense (#1010)

--- apps/beeai-ui/src/components/layouts/AppLayout.tsx
+++ apps/beeai-ui/src/components/layouts/AppLayout.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import type { PropsWithChildren } from 'react';
+import { type PropsWithChildren, Suspense } from 'react';
 
 import { AppHeader } from '#components/AppHeader/AppHeader.tsx';
 import { AgentDetailPanel } from '#modules/agents/components/AgentDetailPanel.tsx';
@@ -13,12 +13,16 @@ import classes from './AppLayout.module.scss';
 export function AppLayout({ children }: PropsWithChildren) {
   return (
     <div className={classes.root}>
-      <AppHeader className={classes.header} />
+      <Suspense>
+        <AppHeader className={classes.header} />
+      </Suspense>
 
       <main className={classes.main} data-route-transition>
         {children}
 
-        <AgentDetailPanel />
+        <Suspense>
+          <AgentDetailPanel />
+        </Suspense>
       </main>
     </div>
   );
