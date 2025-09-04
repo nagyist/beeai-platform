fcadfd9ddbecc2c566e328a392a468aafccb7c4f
Petr Kadlec <petr@puradesign.cz>
2025-09-04 14:48:33 +0200

    feat(ui): enable initial form on chat agents (#1129)
    * feat(ui): enable initial form on chat agents
    
    Signed-off-by: Petr Kadlec <petr@puradesign.cz>
    
    * fixup! hide scroll to bottom button on empty messages
    
    Signed-off-by: Petr Kadlec <petr@puradesign.cz>
    
    * fixup!
    
    Signed-off-by: Petr Kadlec <petr@puradesign.cz>
    
    ---------
    
    Signed-off-by: Petr Kadlec <petr@puradesign.cz>

--- apps/beeai-ui/src/modules/runs/chat/ChatView.tsx
+++ apps/beeai-ui/src/modules/runs/chat/ChatView.tsx
@@ -3,16 +3,22 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { useMemo } from 'react';
+
+import { formExtension } from '#api/a2a/extensions/ui/form.ts';
+import { extractServiceExtensionDemands } from '#api/a2a/extensions/utils.ts';
 import { MainContent } from '#components/layouts/MainContent.tsx';
 import type { Agent } from '#modules/agents/api/types.ts';
 import { SourcesPanel } from '#modules/sources/components/SourcesPanel.tsx';
 
 import { useMessages } from '../../messages/contexts/Messages';
+import { FormRenderView } from '../components/FormRenderView';
 import { RunLandingView } from '../components/RunLandingView';
 import { useAgentRun } from '../contexts/agent-run';
 import { AgentRunProviders } from '../contexts/agent-run/AgentRunProvider';
 import { ChatMessagesView } from './ChatMessagesView';
 
+const formExtensionExtractor = extractServiceExtensionDemands(formExtension);
 interface Props {
   agent: Agent;
 }
@@ -26,15 +32,31 @@ export function ChatView({ agent }: Props) {
 }
 
 function Chat() {
-  const { isPending } = useAgentRun();
+  const { isPending, agent } = useAgentRun();
   const { messages } = useMessages();
+
+  // TODO: move extraction into the agent run context (or a2a client)
+  const formRender = useMemo(() => {
+    const { extensions } = agent.capabilities;
+    const formRender = extensions && formExtensionExtractor(extensions);
+
+    return formRender ?? undefined;
+  }, [agent.capabilities]);
 
   const isLanding = !isPending && !messages.length;
 
   return (
     <>
       <MainContent spacing="md" scrollable={isLanding}>
-        {isLanding ? <RunLandingView /> : <ChatMessagesView />}
+        {isLanding ? (
+          formRender ? (
+            <FormRenderView formRender={formRender} />
+          ) : (
+            <RunLandingView />
+          )
+        ) : (
+          <ChatMessagesView />
+        )}
       </MainContent>
 
       <SourcesPanel />
