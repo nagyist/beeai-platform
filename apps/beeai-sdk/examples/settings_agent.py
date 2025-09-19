5a4f68f90c22710894c7ce32fc1ed2731fd853ed
Tomáš Weiss <tomas.weiss2@gmail.com>
2025-09-19 19:05:03 +0200

    docs: agent settings (#1220)
    * docs: agent settings
    
    Signed-off-by: Tomas Weiss <tomas.weiss2@gmail.com>
    
    * fix: code review
    
    Signed-off-by: Tomas Weiss <tomas.weiss2@gmail.com>
    
    * fix: code review fixes
    
    Signed-off-by: Tomas Weiss <tomas.weiss2@gmail.com>
    
    ---------
    
    Signed-off-by: Tomas Weiss <tomas.weiss2@gmail.com>

--- apps/beeai-sdk/examples/settings_agent.py
+++ apps/beeai-sdk/examples/settings_agent.py
@@ -1,6 +1,7 @@
 # Copyright 2025 © BeeAI a Series of LF Projects, LLC
 # SPDX-License-Identifier: Apache-2.0
 
+import os
 from collections.abc import AsyncGenerator
 from typing import Annotated
 
@@ -9,11 +10,9 @@ from a2a.types import Message
 from beeai_sdk.a2a.extensions.ui.settings import (
     CheckboxField,
     CheckboxGroupField,
-    OptionItem,
     SettingsExtensionServer,
     SettingsExtensionSpec,
     SettingsRender,
-    SingleSelectField,
 )
 from beeai_sdk.a2a.types import RunYield
 from beeai_sdk.server import Server
@@ -40,45 +39,7 @@ async def settings_agent(
                                 default_value=True,
                             )
                         ],
-                    ),
-                    CheckboxGroupField(
-                        id="tools",
-                        fields=[
-                            CheckboxField(
-                                id="tool_1",
-                                label="Tool 1",
-                                default_value=True,
-                            ),
-                            CheckboxField(
-                                id="tool_2",
-                                label="Tool 2",
-                                default_value=False,
-                            ),
-                            CheckboxField(
-                                id="tool_3",
-                                label="Tool 3",
-                                default_value=True,
-                            ),
-                        ],
-                    ),
-                    SingleSelectField(
-                        id="length",
-                        options=[
-                            OptionItem(
-                                label="Short",
-                                value="short",
-                            ),
-                            OptionItem(
-                                label="Medium",
-                                value="medium",
-                            ),
-                            OptionItem(
-                                label="Long",
-                                value="long",
-                            ),
-                        ],
-                        default_value="short",
-                    ),
+                    )
                 ],
             ),
         ),
@@ -99,25 +60,6 @@ async def settings_agent(
         else:
             yield "Thinking is disabled\n"
 
-    tools_field = parsed_settings.values["tools"]
-    if tools_field.type == "checkbox_group":
-        if tools_field.values["tool_1"].value:
-            yield "Tool 1 is enabled\n"
-        else:
-            yield "Tool 1 is disabled\n"
-        if tools_field.values["tool_2"].value:
-            yield "Tool 2 is enabled\n"
-        else:
-            yield "Tool 2 is disabled\n"
-        if tools_field.values["tool_3"].value:
-            yield "Tool 3 is enabled\n"
-        else:
-            yield "Tool 3 is disabled\n"
-
-    length_field = parsed_settings.values["length"]
-    if length_field.type == "single_select":
-        yield f"Length is {length_field.value}\n"
-
 
 if __name__ == "__main__":
-    server.run()
+    server.run(host=os.getenv("HOST", "127.0.0.1"), port=int(os.getenv("PORT", 8000)))
