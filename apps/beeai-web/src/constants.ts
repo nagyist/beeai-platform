df5b935ba8b1c02618b335c20b09160ea43ee51c
Petr Bulánek <bulanek.petr@gmail.com>
2025-11-05 09:39:20 +0100

    chore: change agentstack docs links (#1454)

--- apps/beeai-web/src/constants.ts
+++ apps/beeai-web/src/constants.ts
@@ -3,9 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export const FRAMEWORK_DOCS_LINK = 'https://framework.beeai.dev/introduction/welcome';
+export const FRAMEWORK_DOCS_LINK = 'https://framework.beeai.dev';
 
-export const PLATFORM_DOCS_LINK = 'https://agentstack.beeai.dev/introduction/welcome';
+export const PLATFORM_DOCS_LINK = 'https://agentstack.beeai.dev';
 
 export const GITHUB_LINK = 'https://github.com/i-am-bee';
 
