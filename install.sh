bb3b52d5defa1ce0bd37433df649df1b0b3e084c
Jan Pokorný <JenomPokorny@gmail.com>
2026-02-26 14:08:21 +0100

    chore: update install.sh prerelease version to v0.6.2-rc4
    Signed-off-by: Jan Pokorný <JenomPokorny@gmail.com>

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ set -eu
 
 # These get updated by `mise release`:
 LATEST_STABLE_AGENTSTACK_VERSION=0.6.1
-LATEST_AGENTSTACK_VERSION=0.6.2-rc3
+LATEST_AGENTSTACK_VERSION=0.6.2-rc4
 
 # This gets updated by Renovate:
 # renovate: datasource=python-version depName=python
