c1f8046611875b6df5b5045815d9df8aab79ef9c
Jan Pokorný <JenomPokorny@gmail.com>
2026-03-05 12:44:31 +0100

    fix: keep stable Python version in install.sh
    Signed-off-by: Jan Pokorný <JenomPokorny@gmail.com>

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ LATEST_AGENTSTACK_VERSION=0.6.2-rc6
 
 # This gets updated by Renovate:
 # renovate: datasource=python-version depName=python
-PYTHON_VERSION=3.14
+PYTHON_VERSION=3.13
 
 error() {
     printf "\n💥 \033[31mERROR:\033[0m: Agent Stack installation has failed. Please report the above error: https://github.com/i-am-bee/agentstack/issues\n" >&2
